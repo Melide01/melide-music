63ace43f2140d9dd97181597a8bde1970c1d35e5
Melide01 <56476369+Melide01@users.noreply.github.com>
2025-02-12 13:41:31 -1000

    wow

--- lyrics/script.js
+++ lyrics/script.js
@@ -10,46 +10,70 @@ const song_player = document.getElementById('song_player');
 const lyrics_container = document.getElementById('lyrics_container');
 
 var lyricsdata = [];
+var lyrics;
 
 function toggleClass(el, cla) {
     if (el.classList.contains(cla)) {
-      el.classList.remove(cla);
+        el.classList.remove(cla);
     } else {
-      el.classList.add(cla);
+        el.classList.add(cla);
     }
-  }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     if (songId) {
-        fetch(`../songs/${songId}/lyrics.json`)
-            .then(response => response.json())
-            .then(data => {
-                lyricsdata = data;
-                initialize();
-            })
-            .catch(error => {
-                lyrics_container.textContent = error + `../songs/${songId}/lyrics.json`;
-            });
+        loadLyricsJson();
+        setTimeout(() => {
+            loadLyricsMd();
+        }, 100);    
     }
 });
 
-function initialize() {
-    if (!!!lyricsdata) {
-        setTimeout(() => {
-            if (!!!lyricsdata) {
-                return
-            } else {
-                displayLyrics();
-            };
-        }, 500);
-    } else {
-        displayLyrics();
-    }
+function loadLyricsJson() {
+    fetch(`../songs/${songId}/lyrics.json`)
+        .then(response => response.json())
+        .then(data => {
+            lyricsdata = data;
+        })
+        .catch(error => {
+            lyrics_container.textContent = error + `../songs/${songId}/lyrics.json`;
+        });
 }
 
-var domlines = [];
+function loadLyricsMd() {
+    fetch(`../songs/${songId}/lyrics.md`)
+        .then(response => response.text())
+        .then(data => {
+            lyrics = data;
+            displayLyrics();
+        })
+        .catch(error => {
+            lyrics_container.textContent = error + `../songs/${songId}/lyrics.md`;
+        });
+};
 
+var domlines = [];
+var time_code = [];
 function displayLyrics() {
+    // lyrics_container.textContent = lyricsarr;
+    var ldata = lyrics.split('\n');
+    console.log(ldata);
+
+    var output = "";
+
+    ldata.forEach((e) => {
+        output += e.replace(/^(#\s*)?([^[\]{}]+?)\s*(?:\[(.*?)\])?\s*(?:\{(.*?)\})?\s*$/, (match, hash, mainText, subText, timecode) => {
+            let specialClass = hash ? 'special' : '';
+            let explanationDiv = subText ? `<div class="lyrics_explanation">${subText}</div>` : '';
+
+            time_code.push(timecode?.trim() || "")
+
+            return `<div class="lyrics_line ${specialClass}" onclick="toggleActive(this)">${mainText}${explanationDiv}</div>`;
+        })
+    });
+
+    console.log(time_code);
+
     song_player.querySelector('source').src = lyricsdata.metadata.src;
     song_player.src = lyricsdata.metadata.src;
 
@@ -57,38 +81,24 @@ function displayLyrics() {
     songArtists.textContent = lyricsdata.metadata.artists;
     songGenre.textContent = lyricsdata.metadata.genres;
 
-    var output = "";
-    lyricsdata.lyrics.forEach((e, i) => {
-        output += `<div class="lyrics_line`;
-        if (e.lyrics.startsWith("#")) {
-            output += ` special`;
-        }
-        output += `" onclick="toggleActive(this);">${e.lyrics}`;
-        if (e.explain !== '') {
-            output += `<div class="lyrics_explanation">${e.explain}</div>`;
-        }
-        output += "</div>"
-    });
     lyrics_container.innerHTML = output;
-
-
     domlines = lyrics_container.querySelectorAll(".lyrics_line");
 };
 
 var lastIndex = 0;
-var isLyricsSync = false;
+var isLyricsSync = true;
 function updateSyncLyrics(time) {
     if (!isLyricsSync) return;
-    if (time < 0 || !lyricsdata || !lyricsdata.lyrics.length) return;
+    if (time < 0 || !time_code || !time_code.length) return;
 
-    let lyrics = lyricsdata.lyrics;
+    // let lyrics = lyricsdata.lyrics;
     let newIndex = lastIndex;
 
-    while (newIndex < lyrics.length - 1 && time >= lyrics[newIndex + 1].timecode) {
+    while (newIndex < time_code.length - 1 && time >= time_code[newIndex + 1]) {
         newIndex++;
     }
 
-    while (newIndex > 0 && time < lyrics[newIndex].timecode) {
+    while (newIndex > 0 && time < time_code[newIndex]) {
         newIndex--;
     }
 
@@ -104,7 +114,7 @@ function toggleActive(el) {
     if (el === lastactive) {
         el.classList.remove("active");
         lastactive = undefined;
-        toggleClass(navigation_menu, 'open');
+        /* toggleClass(navigation_menu, 'open'); */
         return;
     }
     if (!!lastactive) {
---
+++ songs/est_elle/lyrics.md
@@ -0,0 +1,13 @@
+# INTRO [J’ai voulu faire cette musique comme si c’était une cassette radio qui se jouait d’où l’effet sonore du début.] {0}
+T'as pris tout mon mal, tu l'as mis ailleurs {3.5}
+Caché dans le sable et ça depuis des heures [La fille de la musique m’a vraiment mis à l’aise, elle est plus agée que moi et elle m’a donner cette maturité que je manquais.] {8.5}
+On voulait bien plus que du sex [] {14.5}
+On s'attendait pas à être vexé [Dans les relations, je m’attendais pas à ce que le désir prennent tellement le dessus sur le reste que ça en devient toxique, il n’y a plus que le sex et c’est ça qui m’a déçu.] {20}
+# PREREFRAIN [Ajout de guitare électrique éffrénnée, la batterie s’accélère] {24.5}
+Mais il est pas certain, faut rebrousser chemin mais t'es pas certaine [Quand elle est partit j’ai pas compris directement. Alors j’ai essayé de me mettre à sa place] {25}
+Bloqué dans le noir, corps contre [] {33.5}
+Est-ce pour un soir où est-ce que j'en veut encore [] {39.3}
+On n'est pas d'accord [Juste avant qu’elle ne parte on faisait encore nos bails et ça s’est finit du jour au lendemain, elle voulait partir et je voulais qu’elle reste.] {44}
+# REFRAIN [Le rythme est cadré mais le chant est long et doux.] {46.5}
+La vie est rose et je me pose si elle est amoureuse {47}
+Ou est-elle un gosse, elle se pose, est-elle amoureuse [Ce sont des questions qu’on se pose tous je crois qu’on l’autre s’en va comme ça.] {58}
