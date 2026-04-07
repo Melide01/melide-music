75d3f234366966e60aa64fb562dd378cc9841423
Melide01 <56476369+Melide01@users.noreply.github.com>
2026-04-06 22:20:09 -1000

    Update LiDe

---
+++ lide/script.js
@@ -0,0 +1,5 @@
+import { handleSearchParam, animateTransition } from '/js/urlEngine.js';
+window.animateTransition = animateTransition;
+
+const project_loader= document.getElementById('project_loader');
+window.project_loader = project_loader;
