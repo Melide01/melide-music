8db5e067ccd4137391bf8fa0df78c29cec86466c
Melide01 <56476369+Melide01@users.noreply.github.com>
2026-07-06 22:26:23 -1000

    Update books

--- books/script.js
+++ books/script.js
@@ -36,8 +36,8 @@ const BOOK_GENRES = {
 }
 
 const TEST_FETCH_BOOKS_DATA = [
-    { "locals": [], "authors": [], "genres": ["GENRES/TRAGEDIE"], "colorize": false, "title": "Proche de la couronne", "description": "Elle n'a pas eu le choix, elle devait survivre dans un monde où le danger est omniprésent. Elle comptera sur son père qui la poussera à rejoindre le Club de chasse de sa région. Qui aurait pu prédire le nombre de décès, lorsqu'ils ont atteint la couronne.", "image_cover": "/assets/Proche de la couronne.png", "link_code": "couronne", "book_data": "book_data.json" },
-    { "locals": [], "authors": [], "genres": ["GENRES/THEATRE"], "colorize": false, "title": "L'homme dans l'écran", "description": "Lors d'un rendez-vous quotidien nocturne, cette famille va vivre le début d'une histoire horrifiante, troublante et presque comique. Mais tout ça n'a rien de drôle pour la petite fille qui a vu l'Homme dans l'écran.", "image_cover": "/assets/placeholder.webp", "link_code": "homme_ecran", "book_data": "book_data.json" }
+    { "locals": [], "authors": [], "genres": ["GENRES/THRILLER"], "colorize": false, "title": "Proche de la couronne", "description": "Elle n'a pas eu le choix, elle devait survivre dans un monde où le danger est omniprésent. Elle comptera sur son père qui la poussera à rejoindre le Club de chasse de sa région. Qui aurait pu prédire le nombre de décès, lorsqu'ils ont atteint la couronne.", "image_cover": "/assets/Proche de la couronne.png", "link_code": "couronne", "book_data": "book_data.json" },
+    { "locals": [], "authors": [], "genres": ["GENRES/THEATRE"], "colorize": false, "title": "L'homme dans l'écran", "description": "Lors d'un rendez-vous quotidien nocturne, cette famille va vivre le début d'une histoire horrifiante, troublante et presque comique. Mais tout ça n'a rien de drôle pour la petite fille qui a vu l'Homme dans l'écran.", "image_cover": "/assets/placeholder.webp", "link_code": "homme_ecran", "book_data": "" }
 ]
 
 function load_books(books) {
@@ -57,19 +57,53 @@ async function load_book(link_code) {
     const book = TEST_FETCH_BOOKS_DATA.filter(v => v.link_code === link_code)[0];
 
     if (book.link_code.startsWith("https://www.")) {
+        
         location.href = book.link_code;
+    
     } else {
-        const res = await fetch(book.book_data);
-        const data = await res.json();
-
         book_section.style.display = "none";
         book_page.classList.add("open");
 
-        console.log(data); 
+        if (!book.book_data) {
+            sections.book_page.innerHTML = "<a>Le contenu de cette page est pour le moment indisponible.</a>";
+            return;
+        };
+        const res = await fetch(book.book_data);
+        const data = await res.json();
+
+        userLang = UrlParams.get("lang", "fr-FR");
+        sections.book_page.innerHTML = "";
+
+        for (const c of data.chapters) {
+            const title_el = document.createElement("h1");
+            title_el.textContent = c.chapter_local_title[userLang];
+            sections.book_page.appendChild(title_el)
+            parse_text_content(c.local_content[userLang]);
+
+        }
     }
 }
 
 function parse_text_content(array_content) {
+    
+    for (const e of array_content) {
+        var element;
+        switch (e.type) {
+            case "title":
+                element = document.createElement('h2');
+                element.textContent = e.text;
+                break;
+            case "image":
+                element = document.createElement('img');
+                element.src = e.src;
+                break;
+            case "text":
+                element = document.createElement('p');
+                element.textContent = e.text;
+                break;
+        }
+        sections.book_page.appendChild(element);
+    }
 
 }
 
@@ -129,12 +163,23 @@ function filter_from_genre(genre) {
     }
 }
 
+document.addEventListener("scroll", (e) => {
+    let time = 2000
+    let current_pos = Math.floor(window.scrollY / document.body.offsetHeight * 100.);
+    
+    var pos = UrlParams.get("pos", -1);
+    
+    if (current_pos === pos) return
+    clearTimeout(document.delay);
+    document.delay = setTimeout(() => {
+        UrlParams.set("pos", current_pos);
+    }, time);
+})
+
 const search_genre = document.getElementById('search_genre');
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const current_lang = UrlParams.get("lang");
     const current_book = UrlParams.get("book");
-    
-    
 
     search_button.addEventListener('click', () => { filter_from_search(search_text_input.value); })
     return_button.addEventListener('click', return_back);
@@ -144,10 +189,12 @@ document.addEventListener('DOMContentLoaded', () => {
     load_local_file();
     
     if (current_book) {
-        load_book(current_book);
+        await load_book(current_book);
     } else {
         load_books(TEST_FETCH_BOOKS_DATA);
     };
+
+    restore_scroll_position();
 
     search_text_input.addEventListener('keydown', (e) => {
         let time = 800
@@ -162,7 +209,6 @@ document.addEventListener('DOMContentLoaded', () => {
     search_genre.innerHTML = "";
     search_genre.appendChild(new Option("Select option", "", true, true))
     for (let g of Object.keys(BOOK_GENRES)) {
-        console.log(loaded_books);
         var has_genre = loaded_books.filter(v => v.genres.includes(g)).length > 0;
         var o = new Option(BOOK_GENRES[g], g);
         search_genre.appendChild(o);
@@ -172,5 +218,16 @@ document.addEventListener('DOMContentLoaded', () => {
     search_genre.addEventListener('change', (e) => {
         filter_from_genre(search_genre.value)
     });
+
+    
 })
 
+function restore_scroll_position() {
+    let pos = UrlParams.get("pos", null);
+    if (pos === null) return;
+        
+    requestAnimationFrame(() => {
+        const max_scroll = document.documentElement.scrollHeight - window.innerHeight;
+        window.scrollTo(0, max_scroll * (pos / 100.));
+    });
+}
